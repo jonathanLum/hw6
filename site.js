907463f0fdc4101e082248c74f5babd6c6a27325
jonathanLum <67062151+jonathanLum@users.noreply.github.com>
2021-03-11 12:58:59 -0500

    Add files via upload

--- site.js
+++ site.js
@@ -17,15 +17,16 @@ app.set('port', 6363);
 
 
 app.get('/',function(req,res){
-  var context = {};
-  mysql.pool.query("SELECT * FROM todo", function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = JSON.stringify(rows);
-    res.render('home',context);
-}));
+	var context = {};
+	mysql.pool.query("SELECT * FROM todo", function(err, rows, fields){
+		if(err){
+		  next(err);
+		  return;
+		}
+		context.results = JSON.stringify(rows);
+		res.render('home',context);
+	});
+});
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
